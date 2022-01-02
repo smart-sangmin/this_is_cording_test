1923f1d7f1472efefd00459532f6f200710188da
smart-sangmin <sangmins930@gmail.com>
2022-01-02 12:33:40 +0900

    상하좌우

--- 4장_구현/4_1.py
+++ 4장_구현/4_1.py
@@ -1,1 +1,14 @@
-input()
+N = int(input())
+c = ('L', 'R', 'U', 'D')
+d = ((0, -1), (0, 1), (-1, 0), (1, 0))
+matrix = [[] * N for _ in range(N)]
+command = input().split()
+location = [0, 0]
+
+for cmd in command:
+    x, y = location[0] + d[c.index(cmd)][0], location[1] + d[c.index(cmd)][1]
+    if x < 0 or x >= N or y < 0 or y >= N:
+        continue
+    location = x, y
+
+print(location[0] + 1, location[1] + 1)
