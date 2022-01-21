d93ce2f3fde45b3448dabb15088bddf9a2a52d73
smart-sangmin <sangmins930@gmail.com>
2022-01-21 20:21:09 +0900

    Initial commit

---
+++ 7장_이진탐색/7_2_떡볶이_떡_만들기.py
@@ -0,0 +1,22 @@
+"""
+4 6
+19 15 10 17
+"""
+N, M = map(int, input().split())
+rice_cake_list = list(map(int, input().split()))
+
+start = 0
+end = max(rice_cake_list)
+
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    for r in rice_cake_list:
+        if r - mid > 0:
+            total += (r - mid)
+    if total >= M:
+        start = mid + 1
+    elif total < M:
+        end = mid - 1
+print(end)
